5c2bdc96114ddb3c8ccbeeed8e33c2b5f81c1755
King17pvp <tuanhai1304@gmail.com>
2024-12-15 00:21:53 +0700

    Nothing lol

--- Test.py
+++ Test.py
@@ -5,6 +5,7 @@ from models.segmentation_models.AttentionUNet import *
 from models.segmentation_models.R2U_Net import *
 from models.segmentation_models.R2AttU_Net import *
 from models.segmentation_models.ResnetUnet import *
+from models.classification_models.ResNet import *
 
 model0 = AttentionUNet()
 model0.load_state_dict(torch.load('weights/AttUNet.pt', map_location=torch.device('cpu')))
@@ -21,13 +22,14 @@ print(model2)
 model3 = ResNetUnet()
 model3.load_state_dict(torch.load('weights/ResNetUNet.pt', map_location=torch.device('cpu')))
 
-model4 = models.resnet18()
+model4 = ResNet18()
 model4.fc = nn.Linear(model4.fc.in_features, 3)
 model4.load_state_dict(torch.load('weights/ResNet18.pt', map_location = torch.device('cpu')), strict= False)
-
 print(model4)
 
-model5 = models.resnet50()
+model5 = ResNet50()
 model5.fc = nn.Linear(model5.fc.in_features, 3)
 model5.load_state_dict(torch.load('weights/ResNet50.pt', map_location = torch.device('cpu')), strict= False)
-print(model5)
+dummy = torch.randn((1, 3, 256, 256))
+print(model5(dummy))
+print(model5(dummy))
