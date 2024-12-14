a19c50de267fb71b44e73ce97b4eb2ecc2c2a0a0
King17pvp <tuanhai1304@gmail.com>
2024-12-14 23:14:18 +0700

    Add test for ResNetUNet

--- Test.py
+++ Test.py
@@ -4,11 +4,10 @@ import torchvision.models as models
 from models.segmentation_models.AttentionUNet import *
 from models.segmentation_models.R2U_Net import *
 from models.segmentation_models.R2AttU_Net import *
+from models.segmentation_models.ResnetUnet import *
 
 model0 = AttentionUNet()
-
 model0.load_state_dict(torch.load('weights/AttUNet.pt', map_location=torch.device('cpu')))
-torch.save(model0.state_dict(), 'weights/AttUNet.pt')
 print(model0)
 
 model1 = R2U_Net()
@@ -18,3 +17,6 @@ print(model1)
 model2 = R2AttU_Net()
 model2.load_state_dict(torch.load('weights/R2AttUNet.pt', map_location=torch.device('cpu')))
 print(model2)
+
+model3 = ResNetUnet()
+model3.load_state_dict(torch.load('weights/ResNetUNet.pt', map_location=torch.device('cpu')))
